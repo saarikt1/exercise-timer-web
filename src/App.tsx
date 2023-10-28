f88f0057484f14fb0e40a7b6df9bc5c672172c74
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2023-10-28 17:57:01 +0300

    Clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import useTimer from "easytimer-react-hook";
 
@@ -10,26 +8,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Timer</h1>
       <div className="card">
         <button onClick={() => timer.start()}>
           {timer.getTimeValues().toString()}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Click the timer to start</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
