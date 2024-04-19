5c5ecbd830147906332792c5a198e43100f7fb32
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2024-04-19 19:07:03 +0300

    Add an error page

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+import { errorMessage } from "./utils.ts";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage(error)}</i>
+      </p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
